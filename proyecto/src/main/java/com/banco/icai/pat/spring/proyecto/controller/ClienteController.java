d7859aeae0efec748d8354db5df8fc4e3945b053
mnara <202206421@alu.comillas.edu>
2025-05-11 17:55:34 +0200

    Login

--- proyecto/src/main/java/com/banco/icai/pat/spring/proyecto/controller/ClienteController.java
+++ proyecto/src/main/java/com/banco/icai/pat/spring/proyecto/controller/ClienteController.java
@@ -1,12 +1,17 @@
 package com.banco.icai.pat.spring.proyecto.controller;
 
+import com.banco.icai.pat.spring.proyecto.entity.Cliente;
 import com.banco.icai.pat.spring.proyecto.model.ClientResponse;
+import com.banco.icai.pat.spring.proyecto.model.LoginRequest;
 import com.banco.icai.pat.spring.proyecto.model.RegisterRequest;
 import com.banco.icai.pat.spring.proyecto.service.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,7 +23,7 @@ public class ClienteController {
     @Autowired
     ClienteService clienteService;
 
-    @PostMapping("/api/users")
+    @PostMapping("/api/royale")
     @ResponseStatus(HttpStatus.CREATED)
     public ClientResponse register(@Valid @RequestBody RegisterRequest register) {
         try {
@@ -27,6 +32,20 @@ public class ClienteController {
             throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage(), e);
         }
     }
+
+    @PostMapping("/api/users/me/session")
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest credentials) {
+        ClientResponse cliente = clienteService.login(credentials.email(), credentials.password());
+        if (cliente == null) throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        ResponseCookie session = ResponseCookie
+                .from("session", cliente.email())
+                .httpOnly(true)
+                .path("/")
+                .sameSite("Strict")
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.SET_COOKIE, session.toString()).build();
+    }
+
 
 
 
