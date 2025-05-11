f6ba8d94c9cee2ee8e8b4bf269f22da42be6ef9f
acobianiregui <anton.cobian.iregui@icloud.com>
2025-05-11 23:49:19 +0200

    Create ClienteControllerIntegrationTest.java

---
+++ proyecto/src/test/java/com/banco/icai/pat/spring/proyecto/controller/ClienteControllerIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.banco.icai.pat.spring.proyecto.controller;
+
+
+import com.banco.icai.pat.spring.proyecto.model.ClientResponse;
+import com.banco.icai.pat.spring.proyecto.model.RegisterRequest;
+import com.banco.icai.pat.spring.proyecto.service.ClienteService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(ClienteController.class)
+public class ClienteControllerIntegrationTest {
+
+    private final String NOMBRE  ="Anton";
+    private final String EMAIL  ="pepelarana@yahho.es";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ClienteService clienteService;
+
+    @Test
+    public void registroClienteOK() throws Exception{
+        
+    }
+}
