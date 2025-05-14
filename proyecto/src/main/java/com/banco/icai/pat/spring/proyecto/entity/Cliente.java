cf2d0f37539548d7b857570f2f5354412e750e48
acobianiregui <anton.cobian.iregui@icloud.com>
2025-05-14 16:33:05 +0200

    Update Cliente.java

--- proyecto/src/main/java/com/banco/icai/pat/spring/proyecto/entity/Cliente.java
+++ proyecto/src/main/java/com/banco/icai/pat/spring/proyecto/entity/Cliente.java
@@ -62,12 +62,21 @@ public class Cliente {
     }
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public String getApellido_1() {
         return apellido;
     }
 
     public void setApellido_1(String apellido_1) {
         this.apellido = apellido_1;
+=======
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido_1(String apellido) {
+        this.apellido = apellido;
+>>>>>>> Stashed changes
 =======
     public String getApellido() {
         return apellido;
